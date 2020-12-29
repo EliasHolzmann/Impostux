7643e106ebeb369a9c3110f11bc1db625bec7521
bartim <bartim@users.noreply.github.com>
2020-12-29 01:06:44 +0100

    Edited some comment

--- impostor_wrapper.sh
+++ impostor_wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -euo pipefail
 
-export LD_PRELOAD="" # otherwise, there are lots of errors after every fork
+export LD_PRELOAD="" # otherwise, there are lots of Steam runtime related errors after every fork
 gnome-terminal --wait -- ~/.local/bin/set_impostor.sh
 exec "$@"
